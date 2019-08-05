11da623ba33a3da8a5418b4124c59c189d2dece8
wingsxdu <beihai@wingsxdu.com>
2019-08-05 16:56:49 +0800

    Complete project migration

--- echarts/total.go
+++ echarts/total.go
@@ -1,33 +1,29 @@
 package echarts
 
 import (
-	"Wade/gorm_mysql"
-	"fmt"
 	"github.com/chenjiandongx/go-echarts/charts"
 	"github.com/labstack/echo/v4"
-	"math/rand"
 	"os"
 )
 
-
-func BarHandler(c echo.Context) error {
+func TotalHandler(c echo.Context) error {
 	page := charts.NewPage(orderRouters("bar")...)
 	page.Add(
-		/*		barBase(),
-				barTitle(),
-				barShowLabel(),
-				barXYName(),
-				barColor(),
-				barSplitLine(),
-				barGap(),
-				barYAxis(),
-				barMultiYAxis(),
-				barMultiXAxis(),
-				barDataZoom(),
-				barReverse(),
-				barStack(),
-				barMark(),
-				barMarkCustom(),*/
+	/*		barBase(),
+			barTitle(),
+			barShowLabel(),
+			barXYName(),
+			barColor(),
+			barSplitLine(),
+			barGap(),
+			barYAxis(),
+			barMultiYAxis(),
+			barMultiXAxis(),
+			barDataZoom(),
+			barReverse(),
+			barStack(),
+			barMark(),
+			barMarkCustom(),*/
 	)
 
 	page.PageTitle = "统计数据"
