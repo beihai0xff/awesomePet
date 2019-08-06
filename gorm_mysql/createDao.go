b8bdfa7f0a6ea6e9b9e6e03786b8d1643f390d27
wingsxdu <baihai@wingsxdu.com>
2019-08-06 17:50:30 +0800

    use Transactions

--- gorm_mysql/createDao.go
+++ gorm_mysql/createDao.go
@@ -5,8 +5,21 @@ import (
 	"fmt"
 )
 
-func Has(uid uint64) bool {
+func HasUser(uid uint64) bool {
 	return !db.Where("uid = ?", uid).First(&User{}).RecordNotFound()
+}
+
+func CreateAccount(user *User, userInfo *UserInfo) error {
+	tx := db.Begin()
+	if err := tx.Create(user).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err := tx.Create(userInfo).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit().Error
 }
 
 func CreateUser(user *User) error {
