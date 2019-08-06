52047c2f786aac1a76ac23b598e19e3151de5095
wingsxdu <baihai@wingsxdu.com>
2019-08-06 16:52:47 +0800

    add model

--- models/mysqlModels.go
+++ models/mysqlModels.go
@@ -1,13 +1,25 @@
 package models
 
+import (
+	"time"
+)
+
+type Model struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+}
+
 //蛇形小写
 type User struct {
+	Model
 	Uid  uint64 `gorm:"primary_key"`
 	Salt string `gorm:"type:char(64); not null"`
 	Key  string `gorm:"type:char(64); not null"`
 }
 
 type UserInfo struct {
+	Model
 	Uid         uint64 `json:"uid"  gorm:"primary_key"`
 	Nickname    string `json:"nickname" gorm:"type:varchar(25); not null"`
 	Sex         uint64 `json:"sex" gorm:"column:sex; not null"`
