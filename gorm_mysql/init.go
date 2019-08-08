cb54bd554e20ffc2e45cf05585bd2ef9b2b23a5c
wingsxdu <beihai@wingsxdu.com>
2019-08-08 17:09:00 +0800

    improve create table

--- gorm_mysql/init.go
+++ gorm_mysql/init.go
@@ -19,41 +19,21 @@ func Init(args *string) {
 	db.LogMode(true)       // 启用Logger，显示详细日志
 	db.SingularTable(true) // 全局禁用表名复数
 	fmt.Println("mysql数据库已连接，检查表结构中...")
-	// 暂时手动创建
-	if !db.HasTable(&User{}) {
-		fmt.Println("表:user不存在，正在创建中")
-		if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").CreateTable(&User{}).Error; err != nil {
-			panic(err)
-		}
-	}
-	if !db.HasTable(&UserInfo{}) {
-		fmt.Println("表:userInfo不存在，正在创建中")
-		if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").CreateTable(&UserInfo{}).Error; err != nil {
-			panic(err)
-		}
-	}
-	if !db.HasTable(&Pet{}) {
-		fmt.Println("表:pet不存在，正在创建中")
-		if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").CreateTable(&Pet{}).Error; err != nil {
-			panic(err)
-		}
-	}
-	CreateTable(&Pic{})
-	/*	if !db.HasTable(&Pic{}) {
-		fmt.Println("表:pic不存在，正在创建中")
-		if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").CreateTable(&Pic{}).Error; err != nil {
-			panic(err)
-		}
-	}*/
+	CreateTable("User", &User{})
+	CreateTable("UserInfo", &UserInfo{})
+	CreateTable("Pet", &Pet{})
+	CreateTable("Pic", &Pic{})
 	//defer db.Close()
 }
 
-func CreateTable(i interface{}) {
-	if !db.HasTable(i) {
-		fmt.Println("表:pic不存在，正在创建中")
-		if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").CreateTable(i).Error; err != nil {
+func CreateTable(name string, table interface{}) {
+	if !db.HasTable(table) {
+		fmt.Printf("表:%s不存在，正在创建中\n", name)
+		if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4").CreateTable(table).Error; err != nil {
+			fmt.Printf("表:%s创建失败，请查看原因：\n", name)
 			panic(err)
 		}
+		fmt.Printf("表:%s创建完成\n", name)
 	}
 
 }
